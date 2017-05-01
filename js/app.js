138cd6dc244c74435b7c6784fc8e68b84ecdbe2b
Marzena Malachowska <malachowska.marzena@gmail.com>
2017-05-01 07:41:42 +0200

    change code, automatic creation of HTML elements (using JS) which store answers - depend on JSON

--- js/app.js
+++ js/app.js
@@ -22,10 +22,11 @@ function loadData(){
     var timer = document.querySelector('.timer');
     var ridle = document.getElementById('ridle');
     var progress = document.getElementById('progress');
-    var answer1 = document.getElementById('ans1').nextSibling;
-    var answer2 = document.getElementById('ans2').nextSibling;
-    var answer3 = document.getElementById('ans3').nextSibling;
-    var answer4 = document.getElementById('ans4').nextSibling;
+    var answersContent = document.getElementById('choices');
+//    var answer1 = document.getElementById('ans1').nextSibling;
+//    var answer2 = document.getElementById('ans2').nextSibling;
+//    var answer3 = document.getElementById('ans3').nextSibling;
+//    var answer4 = document.getElementById('ans4').nextSibling;
     var number = document.getElementById('number');
     var last = document.getElementById('last');
     var bnt = document.getElementById('nextBnt');
@@ -79,15 +80,49 @@ function loadData(){
     //--------------------------------------------------------------
 
     function loadQuestion (currentId) {
+        
+        //---------------------------------------------------
+        answersContent.innerHTML="";
+        var answerLength = question.questions[currentId].answers.length;
+        var newDiv = document.createElement("div");
+        
+        for(var i = 0; i<answerLength; i++) {
+            var newLabel = document.createElement("label");
+            var newInput = document.createElement("input");
+            var newSpan = document.createElement("span");
+            var newBr = document.createElement("br");
+            var questionId = question.questions[currentId].answers[i].id
+            //console.log(newLabel);
+
+            newInput.setAttribute("type", "radio");
+            newInput.setAttribute("name", "question");
+            newInput.setAttribute("id", "ans" + questionId);
+            newInput.setAttribute("value", questionId);
+            newSpan.className = "select";
+            newSpan.innerText = question.questions[currentId].answers[i].answer;
+
+            newLabel.appendChild(newInput);
+            newLabel.insertBefore(newSpan, newInput.nextSibling); 
+            newDiv.appendChild(newLabel);
+            newDiv.appendChild(newBr);
+        }
+        
+        answersContent.appendChild(newDiv);
+        console.log(newDiv);
+                
+
+        //----------------------------------------------------
+        
         ridle.innerText = question.questions[currentId].question;
-        console.log(answer1);
-        answer1.innerText = question.questions[currentId].answers[0].answer;
-        answer2.innerText = question.questions[currentId].answers[1].answer;
-        answer3.innerText = question.questions[currentId].answers[2].answer;
-        answer4.innerText = question.questions[currentId].answers[3].answer;
+//        console.log(answer1);
+//        answer1.innerText = question.questions[currentId].answers[0].answer;
+//        answer2.innerText = question.questions[currentId].answers[1].answer;
+//        answer3.innerText = question.questions[currentId].answers[2].answer;
+//        answer4.innerText = question.questions[currentId].answers[3].answer;
         
         number.innerText = currentId + 1;
         last.innerText = amountOfQuestion;
+        
     }
     
     function loadNextQuestion () {
@@ -118,10 +153,11 @@ function loadData(){
             content.classList.add('hide-me');
             result.classList.remove('hide-me');
             number.innerText = currentId;
-            points.innerText = score;
-            max.innerText = amountOfQuestion;
             currentId = -1;
         }
+        
+        points.innerText = score;
+        max.innerText = amountOfQuestion;
         loadQuestion(currentId);
     }
 
