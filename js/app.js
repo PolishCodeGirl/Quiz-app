bc06333864b2d92c12b8fe7ceed895c06ee15a61
Marzena Malachowska <malachowska.marzena@gmail.com>
2017-05-03 12:29:12 +0200

    adding ES6 fatures

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
 // Get data from JSON
-var questionsData = new XMLHttpRequest();
+let questionsData = new XMLHttpRequest();
 questionsData.open('GET', 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json');
 
-
-//Load data arrow function  
-let loadData = () => {
-    var question = JSON.parse(questionsData.responseText);
-    
-    var QId = 0,
-        score = 0;
-    var numOfQuestion = question.questions.length;
-    
-    // DOM elements
-    var startView = document.querySelector('.welcome'),
+// DOM elements
+    let startView = document.querySelector('.welcome'),
         quizView = document.querySelector('.content'),
         resultView = document.querySelector('.result'),
+        hiView = document.querySelector('.view h2'),
         timer = document.querySelector('.timer'),
         ridle = document.getElementById('ridle'),
         progress = document.getElementById('progress'),
@@ -24,7 +16,21 @@ let loadData = () => {
         points = document.getElementById('score'),
         max = document.getElementById('max'),
         bntNext = document.getElementById('next');
+
+
+let mobile = window.matchMedia("screen and (max-width: 767px)");
+
+if (!mobile.matches){
+    hiView.classList.remove('sg-header-primary--small');    
+    }
+
+//Load data arrow function  
+let loadData = () => {
+    let question = JSON.parse(questionsData.responseText);
     
+    let QId = 0,
+        score = 0;
+    let numOfQuestion = question.questions.length;    
     
     // show desirable view and hide undesirable view
     startView.classList.add('hide-me');
@@ -39,7 +45,7 @@ let loadData = () => {
     //arrow function for loading question from JSON 
     let loadQuestion = (QId) => {
         
-        var numOfAnswers = question.questions[QId].answers.length;
+        let numOfAnswers = question.questions[QId].answers.length;
         
         answersContent.innerHTML="";
         ridle.innerText = question.questions[QId].question;
@@ -47,18 +53,29 @@ let loadData = () => {
         last.innerText = numOfQuestion;
         
         // loop for loading all answers for current question 
-        for(var i = 0; i<numOfAnswers; i++) {
-            var newLabel = document.createElement("label");
-            var newInput = document.createElement("input");
-            var newSpan = document.createElement("span");
-            var newBr = document.createElement("br");
-            var questionId = question.questions[QId].answers[i].id;
+        for(let i = 0; i<numOfAnswers; i++) {
+            let newLabel = document.createElement("label"),
+                newInput = document.createElement("input"),
+                newSpan = document.createElement("span"),
+                newBr = document.createElement("br");
+            
+            let questionId = question.questions[QId].answers[i].id;
 
             newInput.setAttribute("type", `radio`);
             newInput.setAttribute("name", `question`);
             newInput.setAttribute("id", `ans${questionId}`);
             newInput.setAttribute("value", questionId);
-            newSpan.className = "select";
+            
+                    
+            if (mobile.matches){
+                newSpan.setAttribute("class", "sg-text sg-text--small select");
+                ridle.style.overflowY = "scroll";
+                answersContent.style.overflowY = "scroll";
+            }
+            else {
+                newSpan.setAttribute("class", "sg-text select");
+                ridle.classList.remove("sg-text--small");
+            }
             
             newSpan.innerText = question.questions[QId].answers[i].answer;
 
@@ -77,7 +94,7 @@ let loadData = () => {
             return;
         }
         
-        var yourAnswer = yourChoice.getAttribute('value');
+        let yourAnswer = yourChoice.getAttribute('value');
         
         if(question.questions[QId].answers[yourAnswer-1].correct) {
             console.log("poprawna odpowiedz");
@@ -114,8 +131,8 @@ let loadData = () => {
     //--------------------TIMER-------------------------------------------
     // arrow function for counting down the running time
     let setup = () => {
-        var counter = 0,
-            timeLeft = 100; //question.time_seconds;
+        let counter = 0,
+            timeLeft = question.time_seconds;
         timer.innerText = (timeLeft-counter);
 
         
@@ -141,7 +158,7 @@ let loadData = () => {
             
         }
 
-        var countDown = setInterval(timeIt, 1000);
+        let countDown = setInterval(timeIt, 1000);
     }
 
     setup();
@@ -151,7 +168,7 @@ let loadData = () => {
 };
 
 // click events for loading buttons 
-var bntStart = document.getElementById('start'),
+let bntStart = document.getElementById('start'),
     bntAgain = document.getElementById('again');
 
 bntStart.addEventListener('click', loadData);
